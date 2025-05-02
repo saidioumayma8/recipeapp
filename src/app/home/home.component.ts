3ecc45443b51b7d72ab194cb2d445f677e3af3fe
Oumayma Saidi <oumaymasaidi908@gmail.com>
2025-05-02 11:57:46 +0100

    searche

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,27 +2,52 @@ import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../../app/recipeservice.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HttpClientModule, CommonModule],
+  imports: [HttpClientModule, CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-
 export class HomeComponent implements OnInit {
-
-  recipes: any[]=[];
+  recipes: any[] = [];
+  filteredRecipes: any[] = [];
+  searchQuery: string = '';
 
   constructor(private RecipeService: RecipeService) { }
+
   ngOnInit(): void {
-
-    this.RecipeService.getAllRecipes().subscribe((data: any[]) => {
-      this.recipes= data;
-
-      console.log(this.recipes);
+    // Load all recipes on page load
+    this.RecipeService.searchRecipesByName('').subscribe((data: any) => {
+      this.recipes = data || [];
+      this.filteredRecipes = [...this.recipes];
     });
-}
-}
+  }
 
+  onSearchInput(): void {
+    if (!this.searchQuery.trim()) {
+      // If empty, show all again
+      this.filteredRecipes = [...this.recipes];
+    } else {
+      this.RecipeService.searchRecipesByName(this.searchQuery).subscribe((data: any) => {
+        this.filteredRecipes = data || [];
+      });
+    }
+  }
+  filterByCategory() {
+    if (this.selectedCategory) {
+      this.recipeService.getRecipesByCategory(this.selectedCategory).subscribe(data => {
+        this.recipes = data;
+        this.filteredRecipes = this.recipes;
+      });
+    } else {
+      this.getAllRecipes(); // recharger tout si catégorie vide
+    }
+  }
+
+  categories: string[] = ['Beef', 'Chicken', 'Dessert', 'Pasta', 'Seafood', 'Vegetarian'];
+selectedCategory: string = '';
+
+}
--- src/app/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-detail/recipe-detail.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../recipeservice.service';
 
 @Component({
   selector: 'app-recipe-detail',
-  standalone: true,
-  imports: [],
   templateUrl: './recipe-detail.component.html',
-  styleUrl: './recipe-detail.component.css'
 })
-export class RecipeDetailComponent {
+export class RecipeDetailComponent implements OnInit {
+  recipe: any;
 
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.recipeService.getRecipeById(id).subscribe(data => {
+        this.recipe = data;
+      });
+    }
+  }
 }
